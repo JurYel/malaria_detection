58f01ed763a115d537e3ea77a9f2bae62141fcb8
Juriel Botoy <juriel.botoy182@gmail.com>
2021-07-13 20:28:26 +0800

    [SCR-002] - Added model summary

--- scripts/build_architecture.py
+++ scripts/build_architecture.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras.utils import plot_model
 from matplotlib import image as mpimg
 from matplotlib import pyplot as plt
+from tensorflow.keras import backend as K
 from helper_functions import *
+
+tf.compat.v1.disable_eager_execution()
 
 class ConvNetMalaria():
     def __init__(self, num_classes=2):
@@ -35,6 +39,22 @@ class ConvNetMalaria():
 
         model = keras.Model(inputs, logistic_output, name='ConvNetMalaria')
         
+        trainable_count = int(
+            np.sum([K.count_params(p) for p in set(model.trainable_weights)])
+        )
+        non_trainable_count = int(
+            np.sum([K.count_params(p) for p in set(model.non_trainable_weights)])
+        )
+
+        # network summary
+
+        print("\n\n---<summary>---")
+        print("\n Layers: \n\tConvolution2D: {0}\n\tMaxPooling2D: {1}\n\tFully Connected Layers: {2}".format(3, 3, 1))
+        print("\n Total Params: {:,}".format(trainable_count + non_trainable_count))
+        print("\n Trainable Params: {:,}".format(trainable_count))
+        print("\n Non-trainable Params: {:,}".format(non_trainable_count))
+        print("\n\n---</summary>---")
+
         return model
 
     def plot_model_arch(self, model_path="models/malaria_detection/", window_pos=(50, 100)):
